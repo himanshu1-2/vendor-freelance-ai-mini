3433909d6cdc4c7dd1747c04a8e658d06a2b3ac8
himanshu1-2 <himanshu.ajwani96@gmail.com>
2026-09-19 01:06:41 +0530

    qr menu api

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ import { NotFoundPage } from './pages/NotFoundPage'
 export default function App() {
   return (
     <Routes>
-      <Route path="/menu/spice-garden" element={<MenuPage />} />
       <Route element={<AppShell />}>
         <Route index element={<HomePage />} />
+        <Route path="menu/:restaurantSlug" element={<MenuPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
